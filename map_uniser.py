b8f77b9416e34ebbc8a5fe0d6abd604ff3f25968
อนุวัฒน์ ขาวแก้ว <ak.crazysp3660@gmail.com>
2024-07-27 21:15:31 +0700

    add dropdown and callback

--- map_uniser.py
+++ map_uniser.py
@@ -1,30 +1,50 @@
-import plotly.graph_objects as go
 import plotly.express as px
 import pandas as pd
 import json
+from dash import Dash,dcc,html,Input,Output
 
 
 with open('data/univer_lat_long.json', encoding="utf-8") as json_file:
     geo_json= json.load(json_file)
-
-
 uni_list =[]
+dropdown = []
 
-
-
-# print(geo_json)
-for i in geo_json:
+# json to dataframe
+for i in geo_json:        
     uni_list.append([i,geo_json[i]['lat'],geo_json[i]['long']])
+    dropdown.append(i)
 df = pd.DataFrame(uni_list,columns=['name','lat','lon'])
-fig = px.scatter_mapbox(df,
-                        lat='lat',
-                        lon='lon',
-                        hover_name="name",
-                        hover_data=dict(lat = False,lon = False), #not show latitude and longitude
-                        size_max=10,
-                        color_discrete_sequence=["DarkBlue"],
-                        zoom=1)
-fig.update_layout(mapbox_style="open-street-map")
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
-fig.show()
+app = Dash()
+app.layout = [
+    html.H1(children='Tcas Dashboard'),
+    dcc.Dropdown(id='dropdown', placeholder='ชื่อมหาวิทยาลัย', options=dropdown),
+    dcc.Graph(id='map')
+]
+
+@app.callback(
+    Output("map", "figure"),
+    Input("dropdown", "value"),
+)
+
+def display_scatter(dropdown):
+    data = df
+    if dropdown != None:
+        data = df[df['name']==dropdown]
+
+    #mark university location
+    fig = px.scatter_mapbox(data,
+                            lat='lat',
+                            lon='lon',
+                            hover_name="name",
+                            hover_data=dict(lat = False,lon = False), #not show latitude and longitude
+                            size_max=10,
+                            color_discrete_sequence=["DarkBlue"],
+                            zoom=7)
+    fig.update_layout(mapbox_style="open-street-map")
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    return fig
+
+
+if __name__=="__main__":
+    app.run(debug=True)
